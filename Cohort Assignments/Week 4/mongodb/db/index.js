5fc97337e3cda33a229b0b2d797ebb0946ca7f41
Sharma17Deepak <sharmadeepoo30@gmail.com>
2026-05-07 10:21:33 +0530

    changes done

--- Cohort Assignments/Week 4/mongodb/db/index.js
+++ Cohort Assignments/Week 4/mongodb/db/index.js
@@ -1,19 +1,28 @@
 const mongoose = require("mongoose");
 
 //Connect to MongoDB
-mongoose.connect("mongodb+srv://admin:Deepak098%40@cluster0.farjfaj.mongodb.net/");
+mongoose.connect("mongodb+srv://username:password%40@cluster0.farjfaj.mongodb.net/");
 
 //Define schemas
 const AdminSchema = new mongoose.Schema({
-
+    username : String,
+    password : String
 });
 
 const UserSchema = new mongoose.Schema({
-
+    username : String,
+    password : String,
+    purchasedCourses : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Course"
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
-
+    title : String,
+    description : String,
+    imageLink : String,
+    price : Number
 });
 
 const Admin = mongooose.model("Admin",AdminSchema);
